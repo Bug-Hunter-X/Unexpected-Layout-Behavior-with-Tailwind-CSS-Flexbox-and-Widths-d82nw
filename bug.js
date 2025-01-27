a43916028c978a51fcfc7d83f8cb8f529f01915f
BugHunter-X <askzenai@gmail.com>
2025-01-27 03:54:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,13 @@
+```javascript
+// Example showing the error
+<div class="bg-gray-100 p-4">
+  <div class="flex">
+    <div class="w-1/2 bg-red-500">
+      This will cause the problem
+    </div>
+    <div class="w-1/2 bg-blue-500">
+      This might not show up
+    </div>
+  </div>
+</div>
+```
