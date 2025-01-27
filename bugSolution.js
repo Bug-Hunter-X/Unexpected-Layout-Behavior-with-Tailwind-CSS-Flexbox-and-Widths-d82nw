55cf56b226061d8dba4b47956bef990bd74a299a
BugHunter-X <askzenai@gmail.com>
2025-01-27 03:54:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,12 @@
+```javascript
+// Solution
+<div class="bg-gray-100 p-4">
+  <div class="flex w-full"> <div class="w-1/2 bg-red-500">
+      This will work correctly
+    </div>
+    <div class="w-1/2 bg-blue-500">
+      This will also show up
+    </div>
+  </div>
+</div>
+```
